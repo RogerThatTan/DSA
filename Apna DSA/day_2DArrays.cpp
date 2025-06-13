5ab1dc7ccdc62dbf94f51e1dde4de2ca63fddcb4
Tanvir Hassan <101284808+RogerThatTan@users.noreply.github.com>
2025-06-13 20:16:46 +0600

    check

---
+++ Apna DSA/day_2DArrays.cpp
@@ -0,0 +1,15 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main(){
+
+    int matrix[4][3] = {{1,2,3},{4,5,6},{7,8,9}};
+    int rows = 4;
+    int cols = 3;
+
+    matrix[2][1] = 18;
+    cout<<matrix[2][1]<<endl;
+
+
+
+    return 0;
+}
